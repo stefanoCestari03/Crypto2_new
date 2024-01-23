239ca31dc448e8c3780d034a4ff0f84071cc50d4
stefanoCestari03 <128628730+stefanoCestari03@users.noreply.github.com>
2024-01-23 10:00:39 +0100

    Update 1-cestari.py
    graphical update

--- compito2_new/1-cestari.py
+++ compito2_new/1-cestari.py
@@ -5,6 +5,10 @@ from Crypto.Util.Padding import pad
 from Crypto.Hash import MD5
 from Crypto.Random import get_random_bytes
 
+# exaple of graphic
+import tkinter as tk
+from tkinter import filedialog
+
 
 # import of other modules 
 #import lec2
@@ -46,11 +50,12 @@ def generate_key(key_file):
 # encrypt func
 def encrypt_file(in_file,out_file,key):
     try:
-        iv = get_random_bytes(8)
-        cipher = DES3.new(key, DES3.MODE_CBC, iv)
+        # iv = get_random_bytes(8)
+        #cipher = DES3.new(key, DES3.MODE_CBC, iv)
+        cipher = DES3.new(key, DES3.MODE_CBC)
         try:
             with open(in_file, 'rb') as file:
-                iv = file.read(8)
+                #iv = file.read(8)
                 plain_text = file.read()
         except ReadingFileError as err:
             err += " in encrypting situation"
@@ -60,7 +65,8 @@ def encrypt_file(in_file,out_file,key):
         cipher_text = cipher.encrypt(padded_text)
         try:
             with open(out_file + '.enc', 'wb') as file:
-                file.write(iv + cipher_text)
+                # file.write(iv + cipher_text)
+                file.write(cipher_text)
         except WritingFileError as err:
             err += " in encrypting situation"
             print(err)
@@ -69,22 +75,23 @@ def encrypt_file(in_file,out_file,key):
 
 def decrypt_file(in_file,out_file,key):
     try:
-        iv = get_random_bytes(8)
-        cipher = DES3.new(key, DES3.MODE_CBC, iv)
         try:
             with open(in_file, 'rb') as file:
                 iv = file.read(8)
                 text_to_decrypt = file.read()
+            cipher = DES3.new(key, DES3.MODE_CBC, iv)
         except ReadingFileError as err:
             err += " in encrypting situation"
             print(err)
         # padding data
+        
         cipher_text = cipher.decrypt(text_to_decrypt)
         # unpadding text at least 
         plain_text = unpad(cipher_text)
         try:
             with open(out_file + '.dec', 'wb') as file:
-                file.write(iv + plain_text)
+                # file.write(iv + plain_text)
+                file.write(plain_text)
         except WritingFileError as err:
             err += " in encrypting situation"
             print(err)
@@ -93,26 +100,77 @@ def decrypt_file(in_file,out_file,key):
 # main
 
 
-def main():
-    prompt = '''Welcome to  Crypto lab chose a way to Chiper:\n
-    1. Encrypt 3DES\n
-    2. Decrypt 3DES\n
-    \n\ninsert your choice: '''
-    while True:
-        key = ""
-        choice = input(prompt)
-        if choice == '1':
-            in_file = input("insert path of the file to encrypt: ")
-            out_file = input("insert path of the file in output: ")
-            key_file = input("insert path of the keyfile: ")
-            key = generate_key(key_file)
-            encrypt_file(in_file, out_file, key)
-        elif choice == '2':
-            in_file = input("insert path of the file to decrypt: ")
-            out_file = input("inserte path of the file in output: ")
-            decrypt_file(in_file, out_file, key)
-        else:
-            sys.exit()
+# def main():
+#     prompt = '''Welcome to  Crypto lab chose a way to Chiper:\n
+#     1. Encrypt 3DES\n
+#     2. Decrypt 3DES\n
+#     \n\ninsert your choice: '''
+#     # key initialization
+#     key = ""
+#     while True:
+#         choice = input(prompt)
+#         if choice == '1':
+#             in_file = input("insert path of the file to encrypt: ")
+#             out_file = input("insert path of the file in output: ")
+#             key_file = input("insert path of the keyfile: ")
+#             key = generate_key(key_file)
+#             encrypt_file(in_file, out_file, key)
+#             #encrypt_file(in_file, out_file, key_file)
+#         elif choice == '2':
+#             in_file = input("insert path of the file to decrypt: ")
+#             out_file = input("inserte path of the file in output: ")
+#             decrypt_file(in_file, out_file, key)
+#             #decrypt_file(in_file, out_file, key_file)
+#         else:
+#             sys.exit()
         
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+class CryptoApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Crypto Lab")
+
+        self.master.geometry("600x400")
+        
+        self.key = ""
+
+        self.create_widgets()
+        
+
+    def create_widgets(self):
+        self.label = tk.Label(self.master, text="Welcome to Crypto Lab")
+        self.label.pack(pady=10)
+
+        self.encrypt_button = tk.Button(self.master, text="Encrypt 3DES", command=self.encrypt)
+        self.encrypt_button.pack()
+
+        self.decrypt_button = tk.Button(self.master, text="Decrypt 3DES", command=self.decrypt)
+        self.decrypt_button.pack()
+
+    def encrypt(self):
+        in_file = filedialog.askopenfilename(title="Select file to encrypt")
+        out_file = filedialog.asksaveasfilename(title="Select output file")
+        key_file = filedialog.askopenfilename(title="Select key file")
+        self.key = generate_key(key_file)
+        encrypt_file(in_file, out_file, self.key)
+
+    def decrypt(self):
+        if not self.key:
+            print("Please encrypt a file first to obtain the key.")
+            return
+
+        in_file = filedialog.askopenfilename(title="Select file to decrypt")
+        out_file = filedialog.asksaveasfilename(title="Select output file")
+        decrypt_file(in_file, out_file, self.key)
+
+
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = CryptoApp(root)
+    root.mainloop()
